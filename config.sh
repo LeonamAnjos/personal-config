ff261197af5d24daa4eaf4f5d3b200a9289bb8bb
Leonam Anjos <leonam.ricardo@gmail.com>
2019-10-09 17:31:47 +0200

    Bugfix

--- config.sh
+++ config.sh
@@ -5,8 +5,8 @@ apt upgrade
 
 # Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
-apt install -y .google-chrome-stable_current_amd64.deb
-rm .google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome-stable_current_amd64.deb
+rm ./google-chrome-stable_current_amd64.deb
 
 # curl
 apt install -y curl
@@ -18,7 +18,7 @@ apt install -y git
 apt install -y libssl-dev libreadline-dev zlib1g-dev gcc make
 
 # VSCode
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 apt update
 apt install -y code
@@ -37,6 +37,5 @@ rbenv install 2.6.3 && rbenv global 2.6.3
 apt install -y postgresql postgresql-contrib
 
 # nodejs
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt install -y npm
 npm install n
