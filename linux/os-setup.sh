ad70b8871bb51c4275e018cd5d5cc11c5e1ee983
Leonam Santos <leonam.ricardo@gmail.com>
2022-02-27 16:49:48 +0100

    Update linux setup

--- linux/os-setup.sh
+++ linux/os-setup.sh
@@ -1,31 +1,79 @@
 #!/bin/bash
 
-apt update
-apt upgrade
-
-# Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
-rm ./google-chrome-stable_current_amd64.deb
+apt update && apt upgrade
 
 # curl
 apt install -y curl
 
 # git
-add-apt-repository ppa:git-core/ppa
-apt update; apt install -y git
+add-apt-repository ppa:git-core/ppa \
+    && apt update \
+    && apt install -y git
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
+&& apt install -y ./google-chrome-stable_current_amd64.deb \
+&& rm ./google-chrome-stable_current_amd64.deb
+
+# Brave
+apt install -y apt-transport-https \
+&& curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg \
+&& echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list \
+&& apt update \
+&& apt install -y brave-browser
 
 # c compiler
-apt install -y libssl-dev libreadline-dev zlib1g-dev libpq-dev libsqlite3-dev gcc make
+apt install -y      \
+    libssl-dev      \
+    libreadline-dev \
+    zlib1g-dev      \
+    libpq-dev       \
+    libsqlite3-dev  \
+    gcc             \
+    make
 
-# cypress dependecies
-apt install -y xvfb libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libgtk2.0-0:i386
+# miscellaneous
+apt install -y       \
+    copyq            \
+    flameshot        \
+    vlc              \
+    gnome-tweak-tool \
+    htop             \
+    neofetch
 
 # VSCode
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-apt update
-apt install -y code
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -               \
+&& add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" \
+&& apt update     \
+&& apt install -y \
+    code
+
+# nodejs
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+&& source ~/.bashrc \
+&& nvm install node
+
+# apt install -y libpng-dev
+# apt install -y npm
+# npm install -g n
+# sudo n 9.10.1
+npm install --global yarn
+npm install --global typescript
+
+# cypress dependecies
+apt install -y    \
+    xvfb          \
+    libgtk-3-dev  \
+    libnotify-dev \
+    libgconf-2-4  \
+    libnss3       \
+    libxss1       \
+    libasound2    \
+    libgtk2.0-0:i386
+
+# default java-jdk
+apt install -y default-jdk
 
 # rvm
 # better restart computer before using rvm
@@ -50,26 +98,6 @@ curl -fsSL https://github.com/rbenv/rbenv-installer/raw/main/bin/rbenv-doctor | 
 # postgresql
 # apt install -y postgresql postgresql-contrib
 
-# nodejs
-# nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-source ~/.bashrc
-nvm install node
-
-# apt install -y libpng-dev
-# apt install -y npm
-# npm install -g n
-# sudo n 9.10.1
-npm install --global yarn
-npm install --global typescript
-
-# miscellaneous
-apt install -y copyq
-apt install -y flameshot
-apt install -y vlc
-apt install -y gnome-tweak-tool
-apt install -y htop
-
 # add-apt-repository ppa:dawidd0811/neofetch
 # apt update
-apt install -y neofetch
+# apt install -y neofetch
